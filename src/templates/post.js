7970d0b2a53294eb6a18a496961c31ac4eee09e2
Katz <mykatz01@gmail.com>
2020-08-11 19:27:10 -0700

    Add submitform

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,23 +2,30 @@ import React, { Component } from "react"
 import { graphql, Link } from "gatsby"
 import PropTypes from "prop-types"
 
-import Layout from "../components/layout";
+import Layout from "../components/layout"
 import Navbar from "../components/navbar"
+import SubmitForm from "../components/submitform"
 
-import "../styles/post.scss";
+import "../styles/post.scss"
 
 // VERY BASIC WP POST example
 // TODO: make it better in every way
-// TODO: include SEO 
+// TODO: include SEO
 
 class Post extends Component {
   render() {
     const post = this.props.data.wpPost
     let author = {}
-    author.firstName = this.props.data.wpPost?.author?.node?.firstName ? this.props.data.wpPost?.author?.node?.firstName : ""
-    author.lastName = this.props.data.wpPost?.author?.node?.lastName ? this.props.data.wpPost?.author?.node?.lastName : ""
+    author.firstName = this.props.data.wpPost?.author?.node?.firstName
+      ? this.props.data.wpPost?.author?.node?.firstName
+      : ""
+    author.lastName = this.props.data.wpPost?.author?.node?.lastName
+      ? this.props.data.wpPost?.author?.node?.lastName
+      : ""
 
-    const category = this.props.data.wpPost?.categories?.nodes?.length ? this.props.data.wpPost?.categories?.nodes[0].name : "uncategorized"
+    const category = this.props.data.wpPost?.categories?.nodes?.length
+      ? this.props.data.wpPost?.categories?.nodes[0].name
+      : "uncategorized"
     const featuredImage = post?.featuredImage?.node?.sourceUrl
 
     console.log(post)
@@ -26,16 +33,26 @@ class Post extends Component {
 
     return (
       <>
-        <Navbar/>
+        <Navbar />
         <Layout>
           <h1 className="post-title">{post.title}</h1>
-          <div className="post-byline">by {`${author.firstName} ${author.lastName}`.toLowerCase()} • in <Link className="post-category" to="/">{category.toLowerCase()}</Link></div>
-          { featuredImage &&
-            <img src={featuredImage}></img>
-          }
-          <div className="post-excerpt" dangerouslySetInnerHTML={{__html: post.excerpt}}></div>
-          <hr/>
-          <div className="post-body" dangerouslySetInnerHTML={{ __html: post.content }}></div>
+          <div className="post-byline">
+            by {`${author.firstName} ${author.lastName}`.toLowerCase()} • in{" "}
+            <Link className="post-category" to="/">
+              {category.toLowerCase()}
+            </Link>
+          </div>
+          {featuredImage && <img src={featuredImage}></img>}
+          <div
+            className="post-excerpt"
+            dangerouslySetInnerHTML={{ __html: post.excerpt }}
+          ></div>
+          <hr />
+          <div
+            className="post-body"
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          ></div>
+          <SubmitForm />
         </Layout>
       </>
     )
