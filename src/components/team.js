44db6b0b2eaf9321a682d487b898ee72ca121f61
Christopher Tan <christopher.tan17@gmail.com>
2020-08-13 01:30:30 -0700

    About page

---
+++ src/components/team.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+
+const teamArray = [
+  ["Jason Zhao", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Ben Esposito", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Irene Han", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Mark Gardiner", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Enya Lu", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Avery Rogers", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Alessandro Vecchiato", "Content", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Emily Zhong", "Design", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Kaylie Mings", "Design", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Crystal Nattoo", "Design", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Braedon Silvers", "Design", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Rayan Krishnan", "Tech", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Matthew Katz", "Tech", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"],
+  ["Chris Tan", "Tech", "http://52.12.8.45/wp-content/uploads/2020/08/temp_issue_cover.jpg"]
+];
+
+function fadeInUp(elem, delay=0) {
+  return (
+    <ScrollAnimation
+      animateIn="fadeInUp"
+      duration={0.5}
+      animateOnce={true}
+      offset={200}
+      delay={delay}
+    >
+      {elem}
+    </ScrollAnimation>
+  )
+}
+
+function Team() {
+  var team = [];
+  for (let i in teamArray) {
+    team.push(
+      <div className="member" key={i}>
+        <img src={teamArray[i][2]} />
+        <h3>{teamArray[i][0]}</h3>
+        {teamArray[i][1]}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {fadeInUp(<h1>Our Team</h1>)}
+      {fadeInUp(<div id="team">
+        {team}
+      </div>)}
+    </div>
+  )
+}
+
+export default Team
