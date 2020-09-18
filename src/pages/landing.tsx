22409b4863c8c2bc9513274e0fb255814b7bb073
rayankrish <rayankrishnan@live.com>
2020-09-18 00:31:10 -0700

    basic landing functionality - (recover)

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -7,14 +7,13 @@ import SEO from "../components/seo"
 import temp_article_thumbnail from '../images/temp_article_thumbnail.png'
 import temp_issue_cover from '../images/temp_issue_cover.jpg'
 import "../styles/landingV2.scss"
+import { useLandingQuery } from "../hooks/landing_top_query"
 
 const IssuePage = () => {
-
-
     return (
         <>
             <Layout>
-                <SEO title="Isue" />
+                <SEO title="Landing" />
                 <Title />
                 <Articles />
             </Layout>
@@ -23,16 +22,36 @@ const IssuePage = () => {
 }
 
 function Title() {
-    const data = useStaticQuery(pageQuery); // graphql query, see below
-    var index = Math.floor(Math.random()*4)
+    const selected_article = useLandingQuery()
+    var num_articles = selected_article.allWpPost.edges.length
+    var index = Math.floor(Math.random()*num_articles)
+    var selected_article_name = selected_article.allWpPost.edges[index].node.title
+    var selected_article_slug = "/post/" + selected_article.allWpPost.edges[index].node.slug
+    var issue_name = selected_article.allWpPost.edges[index].node.categories.nodes[0].name
+    var other_articles = []
+    for (let i = 0; i <= 3; i++) {
+      if (i != index) {
+        other_articles.push(selected_article.allWpPost.edges[i].node.title)
+      }
+    }
+    var title_variation = Math.floor(Math.random()*2)
+
     return (
         <div>
-            <img id="issue-image" src={temp_issue_cover} alt="issue cover image" />
-            <h1 id="issue-title">
-                {data.allWpPost.edges[index].node.title}
-            </h1>
-            <p id="description">
-                {data.allWpPost.edges[index].node.excerpt}
+            <img id="landing-image" src={temp_issue_cover} alt="issue cover image" />
+            {title_variation == 0 &&
+              <h1 id="landing-title">
+                  Read about <Link to={selected_article_slug} style={{textDecoration: "underline"}}>{selected_article_name}</Link> in our {issue_name} issue
+              </h1>
+            }
+            {title_variation == 1 &&
+              <h1 id="landing-title">
+                  <Link to={selected_article_slug} style={{textDecoration: "underline"}}>{selected_article_name}</Link> is a story in our {issue_name} issue
+              </h1>
+            }
+            <p id="landing-description">
+                Our latest issue, <i>{issue_name}</i>, is released on our website now,
+                and includes article such as {other_articles[0]}, {other_articles[1]}, and {other_articles[2]}.
                 Read the editor's note here &rarr;
             </p>
         </div>
@@ -70,6 +89,7 @@ function Articles() {
 
 export default IssuePage
 
+// Query to get list of articles - random recently published articles not necessarily by issue
 export const pageQuery = graphql`
   query {
     allWpPost(sort: {fields: date, order: DESC}, limit: 4) {
